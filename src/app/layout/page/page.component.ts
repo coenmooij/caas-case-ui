386441cb38eee1507aac60739315044f61c3f7da
Coen Mooij <coenmooij@gmail.com>
2019-05-30 21:24:20 +0200

    add sticky footer, finish basic layout

--- src/app/layout/page/page.component.ts
+++ src/app/layout/page/page.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-page',
-  templateUrl: 'page.component.html'
+  templateUrl: 'page.component.html',
+  styleUrls: ['page.component.scss']
 })
 export class PageComponent {
 }
