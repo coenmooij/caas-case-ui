027774b13bd6d541d2ae07675db3d7dee9dcc73c
Coen Mooij <coenmooij@gmail.com>
2019-05-30 22:11:37 +0200

    Add content for homepage and button for 404

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { Page } from '../../routing/page.enum';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.component.html'
 })
 export class HomeComponent {
+  caseTypesRoute = `/${Page.CASE_TYPES}`;
+  checkerRoute = `/${Page.CHECKER}`;
+  validatorRoute = `/${Page.VALIDATOR}`;
+  converterRoute = `/${Page.CONVERTER}`;
 }
